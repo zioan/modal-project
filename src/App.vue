f770ed3fcd3224c6cb078d309827d14ef2a03038
zioan <zaharia.ioan@gmail.com>
2021-10-25 09:56:07 +0200

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click me</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: "My first vue app :)"
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+    }
   }
 }
+
 </script>
 
 <style>
@@ -22,5 +30,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
